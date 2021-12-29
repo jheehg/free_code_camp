4815eb963788f9a243c3fce3eb0162b48ac550df
Ju hee Hong <juhee.hg@gmail.com>
2021-12-29 21:48:26 +0900

    fixed 19

--- Intermediate_Algorithm_Scripting/arguments-optional.js
+++ Intermediate_Algorithm_Scripting/arguments-optional.js
@@ -1,7 +1,7 @@
-// argument로 두 개의 정수가 들어오면 두 argument 합을 리턴.
-// 만약 하나라도 nubmer 타입이 아니면 undefned 리턴.
-// argument가 하나의 정수만 들어올 경우, 이 argument와의 합을 리턴하는 함수를 만들어 리턴.
-// argument가 하나인데 number 타입이 아니면 undefined 리턴.
+// case 1) argument로 두 개의 정수가 들어오면 두 argument 합을 리턴.
+// case 2) 하나라도 number 타입이 아닐 경우 undefined 리턴.
+// case 3) argument가 하나의 정수로 들어올 경우, 이 argument와의 합을 리턴하는 함수를 만들어 리턴.
+// case 4) case 3에서 생성한 함수의 argument가 number타입이 아닐 경우 undefined 리턴.
 
 const addTogether = (...args) => {
   let func;
